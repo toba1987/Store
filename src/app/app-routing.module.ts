fb1049800abe034ef6784cd1e559f87b7176dc35
momcilo <momcilo87@hotmail.com>
2017-08-25 15:38:40 +0200

    Zadatak 1 Componenet Layout

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
+
+
+
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/buyers',
+        pathMatch: 'full'
+    },
+    {
+        path: 'buyers',
+        component: BuyersComponent
+    },
+    {
+        path: 'products',
+        component: ProductsComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: []
+})
+
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
